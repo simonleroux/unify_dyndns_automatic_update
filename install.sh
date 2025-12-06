876d514754ad9b65ba60022e0d1765cd11667653
Simon Leroux <simon.leroux1@bell.ca>
2025-12-05 19:48:20 -0500

    1

--- install.sh
+++ install.sh
@@ -24,8 +24,8 @@ cat << EOF > "$SERVICE_DIR/custom_dyndns.sh"
 
 printf "Checking if we should change $hostname IP resolution\n"
 
-currentIp=\$(curl 'api.ipify.org' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
-resolvedIp=\$(nslookup $hostname ns2.afraid.org | awk '/^Address: / { print \$2 }')
+currentIp="\$(curl 'api.ipify.org' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')"
+resolvedIp="\$(nslookup $hostname ns2.afraid.org | awk '/^Address: / { print \$2 }')"
 
 printf "Current IP : \$currentIp\nResolved IP : \$resolvedIp\n"
 
